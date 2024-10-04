7bc8fd5c0e0008b2c15d8f9f875a92f7a0424743
breezjirasak <jirasak.tu@ku.th>
2024-10-05 03:19:24 +0700

    update middleware

--- Middlewares/authMiddleware.js
+++ Middlewares/authMiddleware.js
@@ -22,37 +22,4 @@ const authenticateTokenAndRole = (allowedRoles) => {
   };
 };
 
-async function verifyToken(token, secret) {
-  try {
-    const decoded = jwt.verify(token, secret);
-    return decoded;
-  } catch (err) {
-    console.error("Token verification failed:", err.message);
-    return null;
-  }
-}
-
-const verifyCookieToken = async (req, res, next) => {
-  try {
-    const authHeader = req.headers['authorization'];
-    console.log("Authorization Header:", authHeader);
-    if (!authHeader) {
-      return res.status(401).json({ message: "No token provided" });
-    }
-    // Extract the token part from the 'Bearer ' prefix
-    const token = authHeader.split(' ')[1];
-    console.log("Token:", token);
-    const decodedToken = await verifyToken(token, process.env.JWT_SECRET);
-    console.log("Decoded Token:", decodedToken);
-    if (!decodedToken) {
-      return res.status(401).json({ message: "Token verification failed" });
-    }
-    req.user = decodedToken; // Ensure req.user contains all necessary fields
-    next();
-  } catch (err) {
-    console.log("Error in token decode:", err);
-    return res.status(400).json({ message: "Error in token decode" });
-  }
-};
-
-module.exports = { authenticateTokenAndRole, verifyCookieToken };
+module.exports = { authenticateTokenAndRole};
