0ae6f7f25691487fe84a9db4a5c80fb7dd5de634
breezjirasak <jirasak.tu@ku.th>
2025-01-11 02:09:17 +0700

    use mongo as env

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://root:example@localhost:27017/chickcheck?authSource=admin', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
