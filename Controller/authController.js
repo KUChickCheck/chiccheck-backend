c72feccf1607923d93a47c7491592078c5ab2a13
breezjirasak <jirasak.tu@ku.th>
2025-01-12 02:43:30 +0700

    return userinfo

--- Controller/authController.js
+++ Controller/authController.js
@@ -41,7 +41,7 @@ exports.registerStudent = async (req, res) => {
       student_id,
       first_name,
       last_name,
-      email
+      email,
     });
 
     const saveStudent = await newStudent.save();
@@ -62,7 +62,7 @@ exports.registerStudent = async (req, res) => {
 // Student Login
 exports.loginStudent = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, password: inputPassword } = req.body;
 
     const student = await Student.findOne({ username });
 
@@ -70,7 +70,7 @@ exports.loginStudent = async (req, res) => {
       return res.status(400).json({ message: "Invalid username or password" });
     }
 
-    const isPasswordValid = await bcrypt.compare(password, student.password);
+    const isPasswordValid = await bcrypt.compare(inputPassword, student.password);
 
     if (!isPasswordValid) {
       return res.status(400).json({ message: "Invalid username or password" });
@@ -82,7 +82,14 @@ exports.loginStudent = async (req, res) => {
       { expiresIn: "1h" }
     );
 
-    res.status(200).json({ token, message: "Student login successful" });
+    const { password, class_ids, created_at, __v, ...studentInfo} = student.toObject();
+
+    // Send token and user info in response
+    res.status(200).json({
+      token,
+      user: studentInfo,
+      message: "Student login successful",
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal Server Error" });
@@ -127,7 +134,7 @@ exports.registerTeacher = async (req, res) => {
       teacher_id,
       first_name,
       last_name,
-      email
+      email,
     });
 
     const saveTeacher = await newTeacher.save();
